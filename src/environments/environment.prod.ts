9c017b676d1011ce1bc049c70ff19bb45fc072d8
CrunchyLlama <calebvandermaasc@gmail.com>
2022-03-02 10:58:36 -0500

    changed prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCeTprqqdszFXbrlba1jDW9pbxn9d0pBNk",
+    authDomain: "ng-blog-9075a.firebaseapp.com",
+    databaseURL: "https://ng-blog-9075a-default-rtdb.firebaseio.com",
+    projectId: "ng-blog-9075a",
+    storageBucket: "ng-blog-9075a.appspot.com",
+    messagingSenderId: "1058136630544",
+    appId: "1:1058136630544:web:8029fdadcb4ed253bb3195",
+    measurementId: "G-4FLQM4M51D",
+  },
 };
